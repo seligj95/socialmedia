1845b044e19aa114bfb3005a24a966c1eb8bac34
Jordan Selig <seligj95@gmail.com>
2021-02-15 22:35:54 +0000

    inital commit

---
+++ funnelsapp-functions/dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    posts: [
+        {
+            userHandle: 'user',
+            body: 'this is the post body',
+            createdAt: '2021-02-15T21:35:41.261Z',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
---
+++ funnelsapp-functions/functions/index.js
@@ -0,0 +1,115 @@
+const functions = require("firebase-functions");
+const admin =require('firebase-admin');
+const app = require('express')();
+admin.initializeApp();
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDaEQJ_gtKBLuZkjayV1t8MhqW1L_A3dRc",
+    authDomain: "funnel-app-bdd85.firebaseapp.com",
+    projectId: "funnel-app-bdd85",
+    storageBucket: "funnel-app-bdd85.appspot.com",
+    messagingSenderId: "387214425715",
+    appId: "1:387214425715:web:4d95d5363dbee93937f406",
+    measurementId: "G-8294V4087J"
+};
+
+
+const firebase = require('firebase');
+firebase.initializeApp(firebaseConfig);
+
+const db = admin.firestore();
+
+
+app.get('/posts', (req, res) => {
+    db
+        .collection('posts')
+        .orderBy('createdAt', 'desc')
+        .get()
+        .then(data => {
+            let posts = [];
+            data.forEach(doc => {
+                posts.push({
+                    postId: doc.id,
+                    body: doc.data().body,
+                    userHandle: doc.data().userHandle,
+                    createdAt: doc.data().createdAt
+                });
+            });
+            return res.json(posts);
+        })
+        .catch(err => console.error(err));
+});
+
+
+app.post('/post', (req, res) => {
+    const newPost = {
+        body: req.body.body,
+        userHandle: req.body.userHandle,
+        createdAt: new Date().toISOString()  //admin.firestore.Timestamp.fromDate(new Date())
+    };
+
+    db
+        .collection('posts')
+        .add(newPost)
+        .then(doc => {
+            res.json({ message: `document ${doc.id} created successfully` });
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'something went wrong' });
+            console.error(err);
+        });
+});
+
+
+// Signup route
+app.post('/signup', (req, res) => {
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        handle: req.body.handle,
+    };
+
+    // TODO: validate data
+    let token, userId;    
+    db.doc(`/users/${newUser.handle}`).get()
+        .then(doc => {
+            if(doc.exists) {
+                return res.status(400).json({ handle: 'this handle is already taken' })
+            } else {
+                return firebase
+                    .auth()
+                    .createUserWithEmailAndPassword(newUser.email, newUser.password)                
+            }
+        })
+        .then(data => {
+            userId = data.user.uid;
+            return data.user.getIdToken();
+        })
+        .then((idToken) => {
+            token = idToken;
+            const userCredentials = {
+                handle: newUser.handle,
+                email: newUser.email,
+                createdAt: new Date().toISOString(),
+                userId
+            };
+            return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+        })
+        .then(() => {
+            return res.status(201).json({ token });
+        })
+        .catch(err => {
+            console.error(err);
+            if(err.code === 'auth/email-already-in-use') {
+                return res.status(400).json ({ email: 'Email is already in use' });
+            } else {
+                return res.status(500).json({ errror: err.code });
+            }
+        })        
+});
+
+
+// https://baseurl.com/api/
+exports.api  = functions.https.onRequest(app);
