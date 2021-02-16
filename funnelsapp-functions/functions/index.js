acfd7429c0c7d6651c3ea93b009138313185d6a1
Jordan Selig <seligj95@gmail.com>
2021-02-16 03:41:29 +0000

    finished validation and login route

--- funnelsapp-functions/functions/index.js
+++ funnelsapp-functions/functions/index.js
@@ -55,6 +55,17 @@ app.post("/post", (req, res) => {
     });
 });
 
+const isEmail = (email) => {
+  const regEx = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if(email.match(regEx)) return true;
+  else return false;
+}
+
+const isEmpty = (string) => {
+  if(string.trim() === '') return true;
+  else return false;
+}
+
 // Signup route
 app.post("/signup", (req, res) => {
   const newUser = {
@@ -64,6 +75,20 @@ app.post("/signup", (req, res) => {
     handle: req.body.handle,
   };
 
+  let errors = {};
+
+  if(isEmpty(newUser.email)) {
+    errors.email = 'Must not be empty'
+  } else if(!isEmail(newUser.email)) {
+    errors.email = 'Must be a valid email address'
+  }
+
+  if(isEmpty(newUser.password)) errors.password = 'Must not be empty';
+  if(newUser.password !== newUser.confirmPassword) errors.confirmPassword = 'Passwords must match';
+  if(isEmpty(newUser.handle)) errors.handle = 'Must not be empty';
+
+  if(Object.keys(errors).length > 0) return res.status(400).json(errors)
+  
   // TODO: validate data
   let token, userId;
   db.doc(`/users/${newUser.handle}`)
@@ -103,6 +128,36 @@ app.post("/signup", (req, res) => {
       }
     });
 });
+
+app.post('/login', (req, res) => {
+  const user = {
+    email: req.body.email,
+    password: req.body.password
+  };
+
+  let errors = {};
+
+  if(isEmpty(user.email)) errors.email = 'Must not be empty';
+  if(isEmpty(user.password)) errors.password = 'Must not be empty';
+
+  if (Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+  firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+    .then(data =>{
+      return data.user.getIdToken();
+    })
+    .then(token => {
+      return res.json({ token });
+    })
+    .catch(err => {
+      console.error(err);
+      if(err.code === 'auth/wrong-password'){
+        return res.status(403).json({ general: 'Wrong crendentials, please try again' });
+      } else if(err.code === 'auth/user-not-found'){
+        return res.status(403).json({ general: 'User does not exist, please try again or sign up for new account' });
+      } else return res.status(500).json({ error: err.code });
+    })
+})
 
 // https://baseurl.com/api/
 exports.api = functions.https.onRequest(app);
