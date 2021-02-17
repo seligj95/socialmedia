45be2dba98debe7311f56aa85f4213a7c8d0ba27
Jordan Selig <seligj95@gmail.com>
2021-02-17 04:23:42 +0000

    image upload completed

--- funnelsapp-functions/functions/index.js
+++ funnelsapp-functions/functions/index.js
@@ -5,7 +5,7 @@ const app = require('express')();
 const FBAuth = require("./util/fbAuth");
 
 const { getAllPosts, postOnePost } = require("./handlers/posts");
-const { signup, login } = require("./handlers/users");
+const { signup, login, uploadImage } = require("./handlers/users");
 
 // Post routes
 app.get("/posts", getAllPosts);
@@ -14,6 +14,7 @@ app.post("/post", FBAuth, postOnePost);
 // users route
 app.post("/signup", signup);
 app.post("/login", login);
+app.post("/user/image", FBAuth, uploadImage)
 
 // https://baseurl.com/api/
 exports.api = functions.https.onRequest(app);
