6ebbfd4f49db0c267392352caf96ab2985ce8667
Jordan Selig <seligj95@gmail.com>
2021-02-20 22:46:42 +0000

    finished finishing up cloud functions 4:23:46

--- funnelsapp-functions/functions/index.js
+++ funnelsapp-functions/functions/index.js
@@ -30,9 +30,9 @@ exports.api = functions.https.onRequest(app);
 
 exports.createNotificationOnLike = functions.firestore.document('likes/{id}')
   .onCreate((snapshot) => {
-    db.doc(`/posts/${snapshot.data().postId}`).get()
+    return db.doc(`/posts/${snapshot.data().postId}`).get()
       .then(doc => {
-        if(doc.exists){
+        if(doc.exists && doc.data().userHandle !== snapshot.data().userHandle){
           return db.doc(`/notifications/${snapshot.id}`).set({
             createdAt: new Date().toISOString(),
             recipient: doc.data().userHandle,
@@ -43,22 +43,14 @@ exports.createNotificationOnLike = functions.firestore.document('likes/{id}')
           });
         }
       })
-      .then(() => {
-        return
-      })
-      .catch(err => {
-        console.error(err);
-        return;
-      });
+      .catch((err) =>
+        console.error(err));
   });
 
 exports.deleteNotificationOnUnLike = functions.firestore.document('likes/{id}')
   .onDelete((snapshot) => {
-    db.doc(`/notifications/${snapshot.id}`)
+    return db.doc(`/notifications/${snapshot.id}`)
       .delete()
-      .then(() => {
-        return;
-      })
       .catch(err => {
         console.error(err);
         return;
@@ -67,9 +59,9 @@ exports.deleteNotificationOnUnLike = functions.firestore.document('likes/{id}')
 
 exports.createNotificationOnComment = functions.firestore.document('comments/{id}')
   .onCreate((snapshot) => {
-    db.doc(`/posts/${snapshot.data().postId}`).get()
+    return db.doc(`/posts/${snapshot.data().postId}`).get()
     .then(doc => {
-      if(doc.exists){
+      if(doc.exists && doc.data().userHandle !== snapshot.data().userHandle){
         return db.doc(`/notifications/${snapshot.id}`).set({
           createdAt: new Date().toISOString(),
           recipient: doc.data().userHandle,
@@ -80,11 +72,64 @@ exports.createNotificationOnComment = functions.firestore.document('comments/{id
         });
       }
     })
-    .then(() => {
-      return
-    })
     .catch(err => {
       console.error(err);
       return;
     });
   });
+
+exports.onUserImageChange = functions.firestore.document('/users/{userId}')
+  .onUpdate((change) => {
+    console.log(change.before.data());
+    console.log(change.after.data());
+    if(change.before.data().imageUrl !== change.after.data().imageUrl){
+      console.log('image has changed');
+      let batch = db.batch();
+      return db.collection('posts').where('userHandle', '==', change.before.data().handle).get()
+        .then((data) => {
+          data.forEach(doc => {
+            const post = db.doc(`/posts/${doc.id}`);
+            batch.update(post, { userImage: change.after.data().imageUrl });
+          });
+          return batch.commit();
+      });
+    } else return true;
+  });
+
+exports.onPostDelete = functions
+  .firestore.document('/posts/{postId}')
+  .onDelete((snapshot, context) => {
+    const postId = context.params.postId;
+    const batch = db.batch();
+    return db
+      .collection('comments')
+      .where('postId', '==', postId)
+      .get()
+      .then((data) => {
+        data.forEach((doc) => {
+          batch.delete(db.doc(`/comments/${doc.id}`));
+        });
+        return db
+          .collection('likes')
+          .where('postId', '==', postId)
+          .get();
+      })
+      .then((data) => {
+        data.forEach((doc) => {
+          batch.delete(db.doc(`/likes/${doc.id}`));
+        });
+        return db
+          .collection('notifications')
+          .where('postId', '==', postId)
+          .get();
+      })
+      .then((data) => {
+        data.forEach((doc) => {
+          batch.delete(db.doc(`/notifications/${doc.id}`));
+        });
+        return batch.commit();
+      })
+      .catch((err) => console.error(err));
+  });
+
+// TODO: onUserDelete - delete all data like above - use the above function in method, looks like you only have to delete posts and that will trigger the above
