dc96abf54574145c89cf3f0a0d4a93af7047f27c
Jordan Selig <seligj95@gmail.com>
2021-02-20 05:36:42 +0000

    finished create and get notifications, 3:58:11

--- funnelsapp-functions/functions/index.js
+++ funnelsapp-functions/functions/index.js
@@ -1,11 +1,11 @@
 const functions = require("firebase-functions");
-
 const app = require('express')();
-
 const FBAuth = require("./util/fbAuth");
 
+const { db } = require('./util/admin');
+
 const { getAllPosts, postOnePost, getPost, commentOnPost, likePost, unlikePost, deletePost } = require("./handlers/posts");
-const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser } = require("./handlers/users");
+const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser, getUserDetails, markNotificationsRead } = require("./handlers/users");
 
 // Post routes
 app.get("/posts", getAllPosts);
@@ -22,6 +22,69 @@ app.post("/login", login);
 app.post("/user/image", FBAuth, uploadImage);
 app.post("/user", FBAuth, addUserDetails);
 app.get("/user", FBAuth, getAuthenticatedUser);
+app.get('/user/:handle', getUserDetails);
+app.post('/notifications',FBAuth, markNotificationsRead);
 
 // https://baseurl.com/api/
 exports.api = functions.https.onRequest(app);
+
+exports.createNotificationOnLike = functions.firestore.document('likes/{id}')
+  .onCreate((snapshot) => {
+    db.doc(`/posts/${snapshot.data().postId}`).get()
+      .then(doc => {
+        if(doc.exists){
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: 'like',
+            read: false,
+            postId: doc.id
+          });
+        }
+      })
+      .then(() => {
+        return
+      })
+      .catch(err => {
+        console.error(err);
+        return;
+      });
+  });
+
+exports.deleteNotificationOnUnLike = functions.firestore.document('likes/{id}')
+  .onDelete((snapshot) => {
+    db.doc(`/notifications/${snapshot.id}`)
+      .delete()
+      .then(() => {
+        return;
+      })
+      .catch(err => {
+        console.error(err);
+        return;
+      });
+  });
+
+exports.createNotificationOnComment = functions.firestore.document('comments/{id}')
+  .onCreate((snapshot) => {
+    db.doc(`/posts/${snapshot.data().postId}`).get()
+    .then(doc => {
+      if(doc.exists){
+        return db.doc(`/notifications/${snapshot.id}`).set({
+          createdAt: new Date().toISOString(),
+          recipient: doc.data().userHandle,
+          sender: snapshot.data().userHandle,
+          type: 'comment',
+          read: false,
+          postId: doc.id
+        });
+      }
+    })
+    .then(() => {
+      return
+    })
+    .catch(err => {
+      console.error(err);
+      return;
+    });
+  });
