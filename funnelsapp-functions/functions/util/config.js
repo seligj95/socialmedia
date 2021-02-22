c0c938676b46b0843a72149ec67bd036e74baa5a
Jordan Selig <seligj95@gmail.com>
2021-02-22 02:21:54 +0000

    adding remaining files

---
+++ funnelsapp-functions/functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: "${{secret.FIREBASE_PROJECT_KEY}}",
+  authDomain: "funnel-app-bdd85.firebaseapp.com",
+  projectId: "funnel-app-bdd85",
+  storageBucket: "funnel-app-bdd85.appspot.com",
+  messagingSenderId: "387214425715",
+  appId: "1:387214425715:web:4d95d5363dbee93937f406",
+  measurementId: "G-8294V4087J",
+};
