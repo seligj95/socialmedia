4a7f145e6721c5acbaef190253bad5d17cbd8382
Jordan Selig <seligj95@gmail.com>
2021-03-09 04:41:33 +0000

    post dialog 9:47:30

--- funnelsapp-client/src/components/Post.js
+++ funnelsapp-client/src/components/Post.js
@@ -6,7 +6,7 @@ import relativeTime from 'dayjs/plugin/relativeTime';
 import PropTypes from 'prop-types';
 import MyButton from '../util/MyButton';
 import DeletePost from './DeletePost';
-// import PostDialog from './PostDialog';
+import PostDialog from './PostDialog';
 // import LikeButton from './LikeButton';
 // MUI Stuff
 import Card from '@material-ui/core/Card';
@@ -84,11 +84,11 @@ class Post extends Component {
             <ChatIcon color="primary" />
           </MyButton>
           <span>{commentCount} Comments</span>
-          {/* <PostDialog
+          <PostDialog
             postId={postId}
             userHandle={userHandle}
             openDialog={this.props.openDialog}
-          /> */}
+          />
         </CardContent>
       </Card>
     );
